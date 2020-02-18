e1f59029c57d80b193590534f9f285669ff926e9
Ben Koehler <bkoehler85282@gmail.com>
2020-02-17 19:22:14 -0700

    changed file to make more sense

--- client/src/Movies/AddMovie.js
+++ client/src/Movies/AddMovie.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-class CreateMovie extends Component {
+class AddMovie extends Component {
   render() {
     return <></>;
   }
 }
 
-export default CreateMovie;
+export default AddMovie;
